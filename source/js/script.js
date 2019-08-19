49f349ed2c087fae0dfd8dcfed06083204b3deb3
Masha <reflection2008@gmail.com>
2019-08-19 21:22:55 +0300

    Исправлены кавычки в JS, кнопка esc вынесена в переменную

--- source/js/script.js
+++ source/js/script.js
@@ -1,38 +1,39 @@
-var navMain = document.querySelector(".main-navigation");
-var navToggle = document.querySelector(".main-navigation__toggle");
-var link = document.querySelectorAll(".button__order");
-var popup = document.querySelector(".modal");
-var overlayClose = document.querySelector(".overlay");
+var navMain = document.querySelector('.main-navigation');
+var navToggle = document.querySelector('.main-navigation__toggle');
+var link = document.querySelectorAll('.button__order');
+var popup = document.querySelector('.modal');
+var overlayClose = document.querySelector('.overlay');
+var escapeButton = 27;
 
-navMain.classList.remove("main-navigation--nojs");
+navMain.classList.remove('main-navigation--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains("main-navigation--closed")) {
-    navMain.classList.remove("main-navigation--closed");
-    navMain.classList.add("main-navigation--opened");
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
   } else {
-    navMain.classList.add("main-navigation--closed");
-    navMain.classList.remove("main-navigation--opened");
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
   }
 });
 
 for (var i = 0; i < link.length; i++) {
-  link[i].addEventListener("click", function (evt)  {
+  link[i].addEventListener('click', function (evt)  {
     evt.preventDefault();
-    popup.classList.add("modal--show");
+    popup.classList.add('modal--show');
   });
 }
 
-overlayClose.addEventListener("click", function (evt) {
+overlayClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal--show");
+  popup.classList.remove('modal--show');
 })
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode == escapeButton) {
+    if (popup.classList.contains('modal--show')) {
       evt.preventDefault();
-      popup.classList.remove("modal-show");
+      popup.classList.remove('modal--show');
     }
   }
 })
